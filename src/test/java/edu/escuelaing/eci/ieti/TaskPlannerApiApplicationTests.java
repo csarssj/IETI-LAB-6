a47225176af44ec375c68fb91c3115e06911653e
csarssj <cesar.gonzalez-c@mail.escuelaing.edu.co>
2021-03-07 12:12:08 -0500

    :tada: base

---
+++ src/test/java/edu/escuelaing/eci/ieti/TaskPlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.eci.ieti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlannerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
